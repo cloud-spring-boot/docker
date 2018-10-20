a06edca04da1db28da9f6fa2b552443a47170545
Maksym Stepanenko <maksym.stepanenko@opower.com>
2018-10-20 17:28:52 +0300

    tcpdump added

--- dev.sh
+++ dev.sh
@@ -1,2 +1,3 @@
-#!/bin/sh
+#!/bin/bash
+rm -rf pcap/*
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml -p dev up
--- prod.sh
+++ prod.sh
@@ -1,2 +1,3 @@
-#!/bin/sh
+#!/bin/bash
+rm -rf pcap/*
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml -p prod up
